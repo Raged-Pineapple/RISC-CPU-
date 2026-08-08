da16e4da558b4117986268b400bb8286c3f78855
ANIRUDH C <153420003+Raged-Pineapple@users.noreply.github.com>
2026-08-08 15:15:27 +0530

    Add files via upload

---
+++ build/CMakeFiles/Simulator.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dell/RISCV-Simulator/src/BranchPredictor.cpp" "CMakeFiles/Simulator.dir/src/BranchPredictor.cpp.o" "gcc" "CMakeFiles/Simulator.dir/src/BranchPredictor.cpp.o.d"
+  "/home/dell/RISCV-Simulator/src/Cache.cpp" "CMakeFiles/Simulator.dir/src/Cache.cpp.o" "gcc" "CMakeFiles/Simulator.dir/src/Cache.cpp.o.d"
+  "/home/dell/RISCV-Simulator/src/MainCPU.cpp" "CMakeFiles/Simulator.dir/src/MainCPU.cpp.o" "gcc" "CMakeFiles/Simulator.dir/src/MainCPU.cpp.o.d"
+  "/home/dell/RISCV-Simulator/src/MemoryManager.cpp" "CMakeFiles/Simulator.dir/src/MemoryManager.cpp.o" "gcc" "CMakeFiles/Simulator.dir/src/MemoryManager.cpp.o.d"
+  "/home/dell/RISCV-Simulator/src/Simulator.cpp" "CMakeFiles/Simulator.dir/src/Simulator.cpp.o" "gcc" "CMakeFiles/Simulator.dir/src/Simulator.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
